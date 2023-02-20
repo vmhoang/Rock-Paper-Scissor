efb540d67fa33406b8e71a8ae3728cbf390c33f6
Hoang <vumhtran@gmail.com>
2023-02-20 16:52:26 +1100

    Consle js

--- main.js
+++ main.js
@@ -1,44 +1,37 @@
-let r_choice, h_choice, r_point = 0, h_point = 0;
-function randomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-function getComputerchoice(){
-    return randomInteger(1,3);
-    // 1: rock, 2: paper, 3: scissor
+
+
+let r_choice, h_choice,num, result;
+function getComputerChoice(){
+    return Math.floor(Math.random()*3+1);
 }
 
-function human_choice(){
-    if (document.getElementById("rockimg").onclick()){
-        return 1;
+function playRound(h_choice, r_choice){
+    num  = getComputerChoice();
+    switch (num){
+        case 1:
+            r_choice =" ROCK"
+            break;
+        case 2:
+            r_choice = "SCISSOR"
+            break;
+        case 3: 
+            r_choice = "PAPER"
+            break;
     }
-    else if (document.getElementById("paperimg").onclick()){
-        return 2;
+    h_choice = prompt('Rock Paper or Scissor');
+    if (h_choice == r_choice){
+        return "Draw";
     }
-    else if (document.getElementById("scissorimg").onclick()){
-        return 3;
+    else if (r_choice = "ROCK"){
+        return (h_choice.toLowerCase() === "paper") ? "You Win" : "You lose";
+    }
+    else if (r_choice = "PAPER"){
+        return (h_choice.toLowerCase() == "scissor") ? "You Win" : "You lose";
+    }
+    else if (r_choice = "SCISSOR"){
+        return (h_choice.toLowerCase() == "rock") ? "You Win" : "You lose";
     }
 }
 
-function play(h_choice, r_choice){
-    for (let i =0; i <5; i++){
-        r_choice = getComputerchoice();
-        h_choice = human_choice();
-        if ((h_choice - r_choice) === 1){
-            h_point += 1;
-        }
-        else if ((r_choice - h_choice) === 1){
-            h_point += 1;
-        }
-        else if ((h_choice - r_choice) === 2){
-            h_point += 1;
-        }
-        else if ((r_choice - h_choice) === 2){
-            r_point += 1;
-        }
-        console.log(h_choice);
-        console.log(r_choice);
-        console.log(h_point);   
-        console.log(r_point);
-    }
-}
-console.log(play(h_choice,r_choice));
+result = playRound();
+console.log(result);

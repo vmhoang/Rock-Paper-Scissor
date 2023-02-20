13f8c87e79d7331c22c24f1df9bc2deecde39dbd
Hoang <vumhtran@gmail.com>
2023-02-20 23:16:38 +1100

    Second Commit

--- main.js
+++ main.js
@@ -1,44 +1,40 @@
-let r_choice, h_choice, r_point = 0, h_point = 0;
-function randomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-function getComputerchoice(){
-    return randomInteger(1,3);
-    // 1: rock, 2: paper, 3: scissor
-}
 
-function human_choice(){
-    if (document.getElementById("rockimg").onclick()){
-        return 1;
-    }
-    else if (document.getElementById("paperimg").onclick()){
-        return 2;
-    }
-    else if (document.getElementById("scissorimg").onclick()){
-        return 3;
-    }
-}
+let r_choice; 
+let h_choice;
+let result;
+player_choice =  document.querySelectorAll('.gamechoice');
+gameresult = document.getElementById('gameresult');
 
-function play(h_choice, r_choice){
-    for (let i =0; i <5; i++){
-        r_choice = getComputerchoice();
-        h_choice = human_choice();
-        if ((h_choice - r_choice) === 1){
-            h_point += 1;
-        }
-        else if ((r_choice - h_choice) === 1){
-            h_point += 1;
-        }
-        else if ((h_choice - r_choice) === 2){
-            h_point += 1;
-        }
-        else if ((r_choice - h_choice) === 2){
-            r_point += 1;
-        }
-        console.log(h_choice);
-        console.log(r_choice);
-        console.log(h_point);   
-        console.log(r_point);
+player_choice.forEach(button => button.addEventListener('click',() =>{
+    h_choice = button.textContent;
+    r_choice = getcomputerchoice('');
+    result = playRound(h_choice,r_choice);
+    gameresult.textContent = result; 
+}));
+function getcomputerchoice(){
+    const num = Math.floor(Math.random()*3)+1;
+    switch (num){
+        case 1:
+            return " ROCK";
+    
+        case 2:
+            return  "SCISSOR";
+
+        case 3: 
+            return  "PAPER";
     }
 }
-console.log(play(h_choice,r_choice));
+function playRound(h_choice, r_choice){
+    console.log(h_choice);
+    if (h_choice == r_choice){
+    }
+    else if (r_choice = "ROCK"){
+        return (h_choice.toLowerCase() === "paper") ? "You Win" : "You lose";
+    }
+    else if (r_choice = "PAPER"){
+        return (h_choice.toLowerCase() == "scissor") ? "You Win" : "You lose";
+    }
+    else if (r_choice = "SCISSOR"){
+        return (h_choice.toLowerCase() == "rock") ? "You Win" : "You lose";
+    }
+}
